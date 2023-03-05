a3f7c0c720d277cc3f625e1e449700922e80ff86
Julijaand <113669109+Julijaand@users.noreply.github.com>
2023-03-05 10:14:56 +0200

    Update task3_Loop.py
    upgrade after comments

--- task3_Loop.py
+++ task3_Loop.py
@@ -4,9 +4,14 @@ while a <= 100:
     a += 1
     if a % 7 == 0:
         print(a)
+        
+# ====2nd option=========
+for x in range(101):
+    if x % 7 == 0:
+        print(x)
 
 # ======2 task==========
 word = input("Enter a word: ")
-for char in range(len(word) - 1, -1, -1):
-    print(word[char], end="")
+for y in range(len(word) - 1, -1, -1):
+    print(word[y], end="")
 
